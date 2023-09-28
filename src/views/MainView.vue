8a092bc12aa3c0c2ab3784c12e7d24a595b649f9
Enrico Macelloni <enrico.macelloni@unito.it>
2023-09-28 21:59:20 +0200

    01

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -2,7 +2,7 @@
   <!-- inizio Swiper -->
   <div class="swiper mySwiper">
     <div class="swiper-wrapper">
-      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 01</div>
       <div class="swiper-slide">Slide 2</div>
       <div class="swiper-slide">Slide 3</div>
       <div class="swiper-slide">Slide 4</div>
