df0754ef6ae40a61fe896c02877d50cc0db82971
Enrico Macelloni <enrico.macelloni@unito.it>
2023-09-28 21:21:09 +0200

    swiper2

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,4 +1,21 @@
 <template>
+  <!-- inizio Swiper -->
+  <div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+      <div class="swiper-slide">Slide 4</div>
+      <div class="swiper-slide">Slide 5</div>
+      <div class="swiper-slide">Slide 6</div>
+      <div class="swiper-slide">Slide 7</div>
+      <div class="swiper-slide">Slide 8</div>
+      <div class="swiper-slide">Slide 9</div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+  <!-- fine Swiper -->
+
   <div class="about">
     <h1>Il mio negozio</h1>
     <p class="giustificato">
