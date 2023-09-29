2b0290f53cd2b34739ece8800b412f16f7feb2e3
Enrico Macelloni <enrico.macelloni@unito.it>
2023-09-29 22:25:46 +0200

    cose2

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,4 +1,242 @@
 <template>
+  <div
+    class="it-carousel-wrapper it-carousel-landscape-abstract-three-cols splide"
+    data-bs-carousel-splide
+  >
+    <div class="splide__track">
+      <ul class="splide__list">
+        <li class="splide__slide">
+          <div class="it-single-slide-wrapper">
+            <div class="card-wrapper">
+              <div class="card card-img no-after">
+                <div class="img-responsive-wrapper">
+                  <div class="img-responsive">
+                    <div class="img-wrapper">
+                      <img
+                        src="https://via.placeholder.com/480x360/ebebeb/808080/?text=Immagine1"
+                        title="titolo immagine"
+                        alt="descrizione immagine"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body px-3 px-md-4">
+                  <h5 class="card-title">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit…
+                  </h5>
+                  <a class="read-more" href="#">
+                    <span class="text"
+                      >Leggi di più
+                      <span class="visually-hidden"
+                        >Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit…</span
+                      ></span
+                    >
+                    <svg class="icon">
+                      <use
+                        href="http://tema.unito.it/dist/svg/sprite.svg#it-arrow-right"
+                      ></use>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="splide__slide">
+          <div class="it-single-slide-wrapper">
+            <div class="card-wrapper">
+              <div class="card card-img no-after">
+                <div class="img-responsive-wrapper">
+                  <div class="img-responsive">
+                    <div class="img-wrapper">
+                      <img
+                        src="https://via.placeholder.com/480x360/ebebeb/808080/?text=Immagine2"
+                        title="titolo immagine"
+                        alt="descrizione immagine"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body px-3 px-md-4">
+                  <h5 class="card-title">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit…
+                  </h5>
+                  <a class="read-more" href="#">
+                    <span class="text"
+                      >Leggi di più
+                      <span class="visually-hidden"
+                        >Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit…</span
+                      ></span
+                    >
+                    <svg class="icon">
+                      <use
+                        href="http://tema.unito.it/dist/svg/sprite.svg#it-arrow-right"
+                      ></use>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="splide__slide">
+          <div class="it-single-slide-wrapper">
+            <div class="card-wrapper">
+              <div class="card card-img no-after">
+                <div class="img-responsive-wrapper">
+                  <div class="img-responsive">
+                    <div class="img-wrapper">
+                      <img
+                        src="https://via.placeholder.com/480x360/ebebeb/808080/?text=Immagine3"
+                        title="titolo immagine"
+                        alt="descrizione immagine"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body px-3 px-md-4">
+                  <h5 class="card-title">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit…
+                  </h5>
+                  <a class="read-more" href="#">
+                    <span class="text"
+                      >Leggi di più
+                      <span class="visually-hidden"
+                        >Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit…</span
+                      ></span
+                    >
+                    <svg class="icon">
+                      <use
+                        href="http://tema.unito.it/dist/svg/sprite.svg#it-arrow-right"
+                      ></use>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="splide__slide">
+          <div class="it-single-slide-wrapper">
+            <div class="card-wrapper">
+              <div class="card card-img no-after">
+                <div class="img-responsive-wrapper">
+                  <div class="img-responsive">
+                    <div class="img-wrapper">
+                      <img
+                        src="https://via.placeholder.com/480x360/ebebeb/808080/?text=Immagine4"
+                        title="titolo immagine"
+                        alt="descrizione immagine"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body px-3 px-md-4">
+                  <h5 class="card-title">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit…
+                  </h5>
+                  <a class="read-more" href="#">
+                    <span class="text"
+                      >Leggi di più
+                      <span class="visually-hidden"
+                        >Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit…</span
+                      ></span
+                    >
+                    <svg class="icon">
+                      <use
+                        href="http://tema.unito.it/dist/svg/sprite.svg#it-arrow-right"
+                      ></use>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="splide__slide">
+          <div class="it-single-slide-wrapper">
+            <div class="card-wrapper">
+              <div class="card card-img no-after">
+                <div class="img-responsive-wrapper">
+                  <div class="img-responsive">
+                    <div class="img-wrapper">
+                      <img
+                        src="https://via.placeholder.com/480x360/ebebeb/808080/?text=Immagine5"
+                        title="titolo immagine"
+                        alt="descrizione immagine"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body px-3 px-md-4">
+                  <h5 class="card-title">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit…
+                  </h5>
+                  <a class="read-more" href="#">
+                    <span class="text"
+                      >Leggi di più
+                      <span class="visually-hidden"
+                        >Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit…</span
+                      ></span
+                    >
+                    <svg class="icon">
+                      <use
+                        href="http://tema.unito.it/dist/svg/sprite.svg#it-arrow-right"
+                      ></use>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="splide__slide">
+          <div class="it-single-slide-wrapper">
+            <div class="card-wrapper">
+              <div class="card card-img no-after">
+                <div class="img-responsive-wrapper">
+                  <div class="img-responsive">
+                    <div class="img-wrapper">
+                      <img
+                        src="https://via.placeholder.com/480x360/ebebeb/808080/?text=Immagine6"
+                        title="titolo immagine"
+                        alt="descrizione immagine"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body px-3 px-md-4">
+                  <h5 class="card-title">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit…
+                  </h5>
+                  <a class="read-more" href="#">
+                    <span class="text"
+                      >Leggi di più
+                      <span class="visually-hidden"
+                        >Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit…</span
+                      ></span
+                    >
+                    <svg class="icon">
+                      <use
+                        href="http://tema.unito.it/dist/svg/sprite.svg#it-arrow-right"
+                      ></use>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+
   <div class="about">
     <h1>I libri più venduti</h1>
     <p class="giustificato">
