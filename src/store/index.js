54fe2edeff9ad228fffcdf6a4d44fd39ac56d2d0
elena.girotto315 <elena.girotto315@edu.unito.it>
2023-10-01 18:00:38 +0200

    nomi libri

--- src/store/index.js
+++ src/store/index.js
@@ -25,31 +25,31 @@ export default createStore({
     products: [
       {
         id: 1,
-        title: "Libro#1",
+        title: "Karolus. Il romanzo di Carlo Magno",
         image: "https://picsum.photos/200/100?random=11",
         text: "survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing",
       },
       {
         id: 2,
-        title: "Prodotto#2",
+        title: "Il prigioniero dell’interno 7",
         image: "https://picsum.photos/200/100?random=14",
         text: "survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing",
       },
       {
         id: 3,
-        title: "Prodotto#3",
+        title: "Avere tutto",
         image: "https://picsum.photos/200/100?random=7",
         text: "survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing",
       },
       {
         id: 4,
-        title: "Prodotto#4",
+        title: "Vicolo Sant’Andrea 9",
         image: "https://picsum.photos/200/100?random=3",
         text: "survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing",
       },
       {
         id: 5,
-        title: "Prodotto#5",
+        title: "Julie",
         image: "https://picsum.photos/200/100?random=22",
         text: "survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing",
       },
