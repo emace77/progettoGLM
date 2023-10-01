3b0b8a78f21d5654849bfaff3d1a2a5a873ce6c5
elena.girotto315 <elena.girotto315@edu.unito.it>
2023-10-01 19:04:14 +0200

    foto

--- src/store/index.js
+++ src/store/index.js
@@ -27,7 +27,7 @@ export default createStore({
       {
         id: 1,
         title: "Karolus. Il romanzo di Carlo Magno",
-        image: "/images/karolus.jpg",
+        image: "https://picsum.photos/200/100?random=11",
         text: "Che cosa si agitava nel cuore di Karolus, il grande condottiero, quando si preparava a diventare reggente unico del Sacro Romano Impero? Quali sogni – e quali incubi – ne popolavano l'animo?",
       },
       {
