7aa502a4b9c2fe62146e08e714c7192f4e20e779
Enrico Macelloni <enrico.macelloni@unito.it>
2023-10-01 17:51:40 +0200

    car

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -2,7 +2,8 @@
 <template>
   <div class="row">
     <div class="col">
-      <div id="carouselExampleCaptions" class="carousel slide">
+      <h1>I libri più venduti</h1>
+      <div id="carouselExampleCaptions" class="carousel slide p-4 pb-6">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -11,26 +12,42 @@
         <div class="carousel-inner">
           <div class="carousel-item active">
             <a href="#"><img src="../assets/images/img1.png" class="d-block" alt="test"></a>
-            <div class="carousel-caption d-none d-md-block">
-              <h5><a href="#">Maria Rossi</a></h5>
-              <h5><a href="#">Titolo libro 1</a></h5>
+            <div class="carousel-caption text-start">
+              <h5 class="fs-4"><a href="#">Maria Rossi</a></h5>
+              <h5 class="fs-3"><a href="#">Titolo libro 1</a></h5>
               <p><a href="#">13.00 €</a></p>
+              <p class="d-none d-xl-block">Violette Toussaint è guardiana di un cimitero di una cittadina della Borgogna. Ricorda un po' Renée, la protagonista dell'Eleganza del riccio, perché come lei nasconde dietro un'apparenza sciatta una grande personalità e una vita piena di misteri. Durante le visite.</p>
+              <a href="#" class="btn btn-primary">Dettagli</a>
             </div>
           </div>
           <div class="carousel-item">
             <a href="#"><img src="../assets/images/img2.png" class="d-block" alt="test"></a>
-            <div class="carousel-caption d-none d-md-block">
-              <h5><a href="#">Maria Rossi</a></h5>
-              <h5><a href="#">Titolo libro 1</a></h5>
-              <p><a href="#">13.00 €</a></p>
+            <div class="carousel-caption text-start">
+              <h5><a href="#">Rita Verdi</a></h5>
+              <h5><a href="#">Titolo libro 2</a></h5>
+              <p><a href="#">18.00 €</a></p>
+              <p class="d-none d-xl-block">Violette Toussaint è guardiana di un cimitero di una cittadina della Borgogna. Ricorda un po' Renée, la protagonista dell'Eleganza del riccio, perché come lei nasconde dietro un'apparenza sciatta una grande personalità e una vita piena di misteri. Durante le visite.</p>
+              <a href="#" class="btn btn-primary">Dettagli</a>
             </div>
           </div>
           <div class="carousel-item">
             <a href="#"><img src="../assets/images/img3.png" class="d-block" alt="test"></a>
-            <div class="carousel-caption d-none d-md-block">
-              <h5><a href="#">Maria Rossi</a></h5>
-              <h5><a href="#">Titolo libro 1</a></h5>
-              <p><a href="#">13.00 €</a></p>
+            <div class="carousel-caption text-start">
+              <h5><a href="#">Antonio Bianchi</a></h5>
+              <h5><a href="#">Titolo libro 3</a></h5>
+              <p><a href="#">16.00 €</a></p>
+              <p class="d-none d-xl-block">Violette Toussaint è guardiana di un cimitero di una cittadina della Borgogna. Ricorda un po' Renée, la protagonista dell'Eleganza del riccio, perché come lei nasconde dietro un'apparenza sciatta una grande personalità e una vita piena di misteri. Durante le visite.</p>
+              <a href="#" class="btn btn-primary">Dettagli</a>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <a href="#"><img src="../assets/images/img1.png" class="d-block" alt="test"></a>
+            <div class="carousel-caption text-start">
+              <h5 class="fs-4"><a href="#">Gianni Neri</a></h5>
+              <h5 class="fs-3"><a href="#">Titolo libro 4</a></h5>
+              <p><a href="#">15.50 €</a></p>
+              <p class="d-none d-xl-block">Violette Toussaint è guardiana di un cimitero di una cittadina della Borgogna. Ricorda un po' Renée, la protagonista dell'Eleganza del riccio, perché come lei nasconde dietro un'apparenza sciatta una grande personalità e una vita piena di misteri. Durante le visite.</p>
+              <a href="#" class="btn btn-primary">Dettagli</a>
             </div>
           </div>
         </div>
