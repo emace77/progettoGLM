22c67702ff68693c2f4f4e6a2b17584b6daa8383
elena.girotto315 <elena.girotto315@edu.unito.it>
2023-10-03 22:07:41 +0200

    spazio

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -12,7 +12,7 @@
         class="btn btn-blu"
         >Trama</router-link
       >
-      <p class="txt_prezzo mt-4">Prezzo: {{ productData.price }}   Autore: {{ productData.author }}</p>
+      <p class="txt_prezzo mt-4">Prezzo: {{ productData.price }} <br>  Autore: {{ productData.author }}</p>
     </div>
   </div>
 </template>
