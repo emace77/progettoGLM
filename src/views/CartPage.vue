816aa798f3f2e60a6e4e316bcb46a2affab1e4a0
elena.girotto315 <elena.girotto315@edu.unito.it>
2023-10-05 17:19:03 +0200

    Collegamenti carrello

---
+++ src/views/CartPage.vue
@@ -0,0 +1,15 @@
+<!-- eslint-disable -->
+<!-- components/CartPage.vue -->
+<template>
+    <div>
+      <h2>Carrello</h2>
+      <!-- contenuto del tuo carrello -->
+    </div>
+  </template>
+<!-- eslint-disable -->
+  <script>
+  /* eslint-disable */
+  export default {
+    name: 'CartPage'
+  }
+  </script>
