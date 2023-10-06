c039429662d73c1c1da5d64043d25bec68343f30
Rosalba Limongiello <rosalba.limongiello@edu.unito.it>
2023-10-06 23:19:05 +0200

    prova funzionamento login

--- src/views/LogView.vue
+++ src/views/LogView.vue
@@ -1,9 +1,9 @@
 <!-- eslint-disable -->
-<!-- components/LogPage.vue -->
+<!-- components/LogView.vue -->
 <template>
   <div>
     <h2>Login</h2>
-    <form class="form-signin mx-auto">
+    <form class="form-signin mx-auto" @submit.prevent="onSubmit">
       <label for="inputEmail" class="sr-only">Email</label>
       <input id="inputEmail" type="email" class="form-control" placeholder="Email" v-model="email" required/>
 
@@ -18,40 +18,80 @@
 
       <button class="btn btn-blu btn-sm btn-block" type="submit">Entra</button>
 
+      <!-- Messaggio registrazione avvenuta con successo -->
+      <p v-if="accesso">{{ accesso }}</p>
+
+       <!-- Messaggio di errore -->
+       <p v-if="erroreLogin" class="errore">{{ erroreLogin }}</p>
+
       <p>
-        Non hai un account? <a href="#">Registrati qui</a> <br>
-        <a href="#">Hai dimenticato la password?</a>
+      Non hai un account? <a href="#">Registrati qui</a> <br>
+      <a href="#">Hai dimenticato la password?</a>
       </p>
-    </form>
+      </form>
   </div>
 </template>
 <!-- eslint-disable -->
-  <script>
-  /* eslint-disable */
-  export default {
-  name: 'LogPage',
+<script>
+export default {
+  name: 'LogView',
   data() {
     return {
+      // variabili
       email: null,
       password: null,
       remember: false,
+      accesso: false,
+      erroreLogin: '', 
     };
   },
+  methods: {
+    onSubmit() {
+      const email = this.email;
+      const password = this.password;
+
+      // Verifica se i dati di accesso sono validi
+      if (this.isLoginValid(email, password)) {
+        this.accesso = true;
+        this.accesso = 'Login avvenuto con successo!';
+        this.erroreLogin = ''; // Resetta il messaggio di errore
+      } else {
+        this.accesso = false;
+        this.erroreLogin = 'Email e/o password non validi. Riprova.';
+      }
+    },
+
+    //Verificare la correttezza dei dati
+    isLoginValid(email, password) {
+      if (!email || !password) {
+        return false;
+      }
+      if (password.length < 6) {
+        return false;
+      }
+      return true;
+    },
+  },
 };
-  </script>
+</script>
+<!-- eslint-disable -->
 <style>
 .form-signin {
-    width: 100%;
-    max-width: 330px;
-    padding: 15px;
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
 }
 
-.btn-blu{
-  background-color:#006fba ;
-  color:#ffff;
+.btn-blu {
+  background-color: #006fba;
+  color: #ffff;
 }
-.btn-blu:hover{
-  background-color:#00aeef ;
-  color:#ffff;
+
+.btn-blu:hover {
+  background-color: #00aeef;
+  color: #ffff;
+}
+.errore {
+  color: red;
 }
 </style>
