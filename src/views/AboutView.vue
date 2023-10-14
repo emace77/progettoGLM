efad85c17b876a95a716df213afc3bc2fb0cdb75
elena.girotto315 <elena.girotto315@edu.unito.it>
2023-10-14 11:58:15 +0200

    testo come funziona il servizio

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,27 @@
 <!-- eslint-disable -->
 <template>
-    <h1>Come funziona il nostro servizio</h1>
+    <h1>Chi siamo!</h1>
     <img src="../assets/images/hero.png" class="d-block mx-lg-auto img-fluid" alt="Image by pch.vector on Freepik" loading="lazy">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed tempor tortor, sit amet sodales nulla. Vivamus posuere gravida mauris vel pretium. Phasellus faucibus turpis arcu, eu facilisis tellus convallis vitae. Mauris venenatis sagittis nisi, a commodo massa tincidunt nec. Suspendisse potenti. Nunc augue quam, tincidunt a laoreet consectetur, facilisis at felis. Cras interdum scelerisque eros, eu aliquam odio mollis ut.</p>
-    <p>Donec consequat volutpat felis, non congue quam facilisis id. Praesent pharetra metus augue, in mollis nibh ullamcorper eget. Suspendisse volutpat massa ligula. Pellentesque consequat finibus enim, et euismod nulla iaculis quis. Vestibulum quis gravida lorem. Cras in feugiat lectus. Duis ex est, rutrum eget suscipit at, posuere accumsan eros. Aliquam hendrerit pulvinar diam sed rutrum. Duis at tellus lacinia, lobortis lectus a, mollis ipsum. Phasellus lacus purus, sagittis eget fermentum et, tempus commodo orci. Sed consectetur sed tellus imperdiet ullamcorper. Cras ac massa sit amet urna molestie condimentum ornare at nibh. Phasellus viverra finibus orci, vel viverra augue rutrum a. Phasellus lectus purus, hendrerit sed mi a, rhoncus ultricies massa. Vestibulum laoreet accumsan leo id rhoncus. Morbi condimentum, nunc quis feugiat porttitor, magna ligula fermentum tellus, vitae convallis neque risus a neque.</p>
+    <p>BookVue non è solo una grande libreria di Torino, ma un luogo dedicato alla narrazione. Nasce dalla passione per le storie e per condividerle meglio ha deciso di ampliare il suo orizzonte e avvicinarsi a tutti gli amanti della lettura aprendo un nuovissimo e-commerce.  </p>
+    <h2> Come funziona il nostro servizio:</h2>
+    <p>Il nostro sito è semplice e intuitivo, pensato per offrire un'esperienza di navigazione piacevole. Al suo interno potete trovare:</p>
+    <ol>
+        <li><p class="pagina">Pagina libri:</p><p>Navigando nella sezione "Libri", è possibile sfogliare l'intera collezione di titoli selezionati. Da opere classiche a nuovi bestseller, c'è qualcosa per tutti gli appassionati della narrativa italiana.</p></li>
+        <li><p class="pagina">Pagina Dettaglio:</p><p>Ogni libro ha una sua pagina dedicata, dove è presente una descrizione dettagliata, le recensioni dei lettori e informazioni sull'autore. Da qui, con un semplice click, si può aggiungere il libro nel proprio carrello.</p></li>
+        <li><p class="pagina">Carrello:</p><p>Una volta selezionati i libri preferiti, nella pagina del carrello sarà presente un elenco di ciò che è stato selezionato e nel caso in cui non si fosse convinti della propria scelta si potrà azzerare senza alcun problema. </p></li>
+        <li><p class="pagina">Pagina di ricerca:</p><p>Se hai in mente un titolo specifico o desideri esplorare categorie particolari, la funzione di ricerca avanzata ti aiuterà a trovare esattamente ciò che stai cercando.</p></li>
+        <li><p class="pagina">Promozioni e offerte speciali:</p><p>BookVue pensa anche ai giovani e agli studenti! Con un coupon di 20 euro per gli studenti delle superiori, leggere non è mai stato così conveniente. E non è tutto: partecipa al nostro quiz per avere la possibilità di vincere un libro! Se, invece, desideri fare spazio nella tua libreria, offriamo anche la possibilità di vendere libri usati, dando loro una seconda vita tra le mani di un altro appassionato lettore.</p></li>
+    </ol>
+    <h3 class="titoletto text-center pt-5 pb-5"> Ora che sai tutto, dai spazio alla tua passione per la lettura e trova i tuoi libri preferiti!</h3>
 </template>
 <!-- eslint-disable -->
+<style>
+.pagina{
+    color:#006fba;
+    font-size:medium;
+    font-weight: 700;
+}
+.titoletto{
+    color: #9e5c00;
+}
+</style>
