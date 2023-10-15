419b8a30d4036123195d5a47b1e93cd3156b1f12
Enrico Macelloni <enrico.macelloni@unito.it>
2023-10-15 16:05:54 +0200

    fix foto

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -65,7 +65,7 @@
     </div>
   </div>
   <div class="row">
-    <div class="px-4 py-5 my-5 text-center blue-light">
+    <div class="px-4 py-5 my-2 my-lg-5 text-center blue-light">
     <h1 class="display-6 fw-bold">Sconto del 10% sul primo ordine</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">Registrati gratuitamente e ottieni subito un codice sconto per il tuo primo acquisto.</p>
