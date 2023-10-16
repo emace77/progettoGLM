3044e8f641995b3b0c18bb4c152d11cee2be3adc
Enrico Macelloni <enrico.macelloni@unito.it>
2023-10-16 22:09:45 +0200

    promo

--- src/views/CardsView.vue
+++ src/views/CardsView.vue
@@ -5,7 +5,7 @@
   v-for="card in cards" 
   :key="card.id">
     <router-link :key="card.id" :to="'/card/' + card.id" @click="showCard(card)">
-      <img :src="card.image" class="card-img-top mb-2" :alt="card.alt" />
+      <img :src="card.image" class="card-img-top mb-2 w-50" :alt="card.alt" />
     </router-link>
     <h5>
       <!-- con l'evento @click invoco mostraDettaglio -->
